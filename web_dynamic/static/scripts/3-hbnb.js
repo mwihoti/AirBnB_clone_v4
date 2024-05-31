cdd355d410de4cc6ed83dba6bf26201f9a7f459c
mwihoti <danielmwihoti@gmail.com>
2024-05-31 14:03:52 +0300

    Fetch places

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,46 @@
+$(document).ready(function () {
+    const amenities = {};
+  
+    $('input[type="checkbox"]').change(function () {
+      if (this.checked) {
+        amenities[$(this).data('id')] = $(this).data('name');
+      } else {
+        delete amenities[$(this).data('id')];
+      }
+      $('.amenities h4').text(Object.values(amenities).join(', '));
+    });
+  
+    $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+      if (data.status === 'OK') {
+        $('#api_status').addClass('available');
+      } else {
+        $('#api_status').removeClass('available');
+      }
+    });
+  
+    $.ajax({
+      url: 'http://0.0.0.0:5001/api/v1/places_search/',
+      type: 'POST',
+      data: JSON.stringify({}),
+      contentType: 'application/json',
+      success: function (data) {
+        for (const place of data) {
+          $('.places').append(
+            `<article>
+              <div class="title_box">
+                <h2>${place.name}</h2>
+                <div class="price_by_night">$${place.price_by_night}</div>
+              </div>
+              <div class="information">
+                <div class="max_guest">${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}</div>
+                <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}</div>
+                <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>
+              </div>
+              <div class="description">${place.description}</div>
+            </article>`
+          );
+        }
+      }
+    });
+  });
+  
